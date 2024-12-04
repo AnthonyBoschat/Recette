5097f11452a2388069c3677884f7c5e5ec18f38a
Anthony Boschat <anthonyboschat.dev@hotmail.com>
2024-12-04 22:01:53 +0100

    init

--- Frontend/src/appollo-client.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-
-const client = new ApolloClient({
-    uri: 'http://localhost:8000/graphql/', // Remplace l'URL par celle de ton endpoint GraphQL
-    cache: new InMemoryCache(),
-});
-
-export default client;

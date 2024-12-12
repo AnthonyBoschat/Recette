f031d6ff02f244471764e57af70cda5d42e6349f
Anthony Boschat <anthonyboschat.dev@hotmail.com>
2024-12-12 23:18:43 +0100

    • Ajout du bouton de submit
    • Les bouton de durée réagissent correctement
    
    • Style wrap pour les conteneurs

--- Frontend/src/Layout/AddRecipe/index.tsx
+++ Frontend/src/Layout/AddRecipe/index.tsx
@@ -1,4 +1,4 @@
-import { BaseSyntheticEvent, MouseEventHandler, useReducer } from "react"
+import { BaseSyntheticEvent, FormEventHandler, MouseEventHandler, useReducer } from "react"
 import s from "./style.module.scss"
 import IDService from "@Services/IDService"
 
@@ -12,9 +12,9 @@ interface instructionType{
     id:number
     step:number
     sentence:string
-    hour:number
-    minute:number
-    seconde:number
+    hour:number|string
+    minute:number|string
+    seconde:number|string
 }
 
 interface materialType{
@@ -45,25 +45,20 @@ type recipeAction =
 export default function AddRecipeLayout(){
 
     const removeNullNumber = (e:BaseSyntheticEvent) => {
-        console.log(e.currentTarget.value)
         if(e.currentTarget.value === "-"){
             e.currentTarget.value = ""
         }
     }
     const addNullNumber = (e:BaseSyntheticEvent) => {
-        console.log(e.currentTarget.value)
         if(e.currentTarget.value === "" || e.currentTarget.value === "0"){
             e.currentTarget.value = "-"
         }
     }
 
-    const nullNumber = (value:number) => {
-        if(value === 0){
-            return "-"
-        }
-    }
-
     const checkNumber = (durationValue:string) => {
+        if(durationValue === ""){
+            return true
+        }
         return /^\d+$/.test(durationValue)
     }
 
@@ -81,9 +76,9 @@ export default function AddRecipeLayout(){
             {id:3, name:"Levure", weight:8},
         ],
         instructions:[
-            {id:0, step:1, sentence:"Mettre la farine et l’eau dans la cuve et pétrir pendant 3 minutes jusqu’à obtenir une pâte bâtarde.", hour:0, minute:0, seconde:0},
-            {id:1, step:2, sentence:"Laisser reposer la pâte dans la cuve couvert d’un linge pendant 30 minutes.", hour:0, minute:0, seconde:0},
-            {id:2, step:3, sentence:"Incorporer la levure dans la pâte.", hour:0, minute:0, seconde:0},
+            {id:0, step:1, sentence:"Mettre la farine et l’eau dans la cuve et pétrir pendant 3 minutes jusqu’à obtenir une pâte bâtarde.", hour:"-", minute:"-", seconde:"-"},
+            {id:1, step:2, sentence:"Laisser reposer la pâte dans la cuve couvert d’un linge pendant 30 minutes.", hour:"-", minute:"-", seconde:"-"},
+            {id:2, step:3, sentence:"Incorporer la levure dans la pâte.", hour:"-", minute:"-", seconde:"-"},
         ]
     }
 
@@ -102,27 +97,24 @@ export default function AddRecipeLayout(){
             case "SET_INGREDIENT_WEIGHT":
                 return {...state, ingredients:state.ingredients.map(ingredient => ingredient.id === action.payload.id ? {...ingredient, weight:action.payload.weight} : ingredient)}
             case "ADD_INSTRUCTION":
-                return {...state, instructions:[...state.instructions, {id:IDService.getNextID(state.instructions), step:getNextStep(state.instructions), sentence:"", hour:0, minute:0, seconde:0}]}
+                return {...state, instructions:[...state.instructions, {id:IDService.getNextID(state.instructions), step:getNextStep(state.instructions), sentence:"", hour:"-", minute:"-", seconde:"-"}]}
             case "SET_INSTRUCTION_SENTENCE":
                 return {...state, instructions:state.instructions.map(instruction => instruction.id === action.payload.id ? {...instruction, sentence:action.payload.sentence} : instruction)}
             case "SET_INSTRUCTION_HOUR":
-                console.log(checkNumber(action.payload.value))
-                console.log(state)
                 if(checkNumber(action.payload.value)){
-                    console.log("GO")
-                    return {...state, instructions:state.instructions.map(instruction => instruction.id === action.payload.id ? {...instruction, hour:parseInt(action.payload.value)} : instruction)}
+                    return {...state, instructions:state.instructions.map(instruction => instruction.id === action.payload.id ? {...instruction, hour:action.payload.value !== "" ? parseInt(action.payload.value) : "-"} : instruction)}
                 }else{
                     return state
                 }
             case "SET_INSTRUCTION_MINUTE":
                 if(checkNumber(action.payload.value)){
-                    return {...state, instructions:state.instructions.map(instruction => instruction.id === action.payload.id ? {...instruction, minute:parseInt(action.payload.value)} : instruction)}
+                    return {...state, instructions:state.instructions.map(instruction => instruction.id === action.payload.id ? {...instruction, minute:action.payload.value !== "" ? parseInt(action.payload.value) : "-"} : instruction)}
                 }else{
                     return state
                 }
             case "SET_INSTRUCTION_SECONDE":
                 if(checkNumber(action.payload.value)){
-                    return {...state, instructions:state.instructions.map(instruction => instruction.id === action.payload.id ? {...instruction, seconde:parseInt(action.payload.value)} : instruction)}
+                    return {...state, instructions:state.instructions.map(instruction => instruction.id === action.payload.id ? {...instruction, seconde:action.payload.value !== "" ? parseInt(action.payload.value) : "-"} : instruction)}
                 }else{
                     return state
                 }
@@ -133,20 +125,24 @@ export default function AddRecipeLayout(){
 
     const [recipeState, recipeDispatch] = useReducer(recipeStateReducer, recipeInitialState)
 
-    
+    const handleSubmit = (e:BaseSyntheticEvent) => {
+        e.preventDefault()
+        console.log("submit")
+    }
 
 
 
     return(
-        <div className={s.mainContainer}>
+        <form onSubmit={handleSubmit} className={s.mainContainer}>
 
-            <div className={s.part}>
+            {/* Nom de la recette */}
+            <div className={`${s.part} ${s.header}`}>
                 <div className={`${s.section}`}>
                     <label htmlFor="">Nom</label>
                     <input onChange={(e) => recipeDispatch({type:"SET_RECIPE_NAME", payload:e.currentTarget.value})} type="text" className={`${s.dark}`} value={recipeState.recipeName} />
                 </div>
+                <input type="submit" className={`${s.dark} ${s.submit}`} value={"Enregistrer la recette"} />
             </div>
-
             <div className={s.part}>
 
                 {/* Materiel */}
@@ -154,7 +150,7 @@ export default function AddRecipeLayout(){
                     <label htmlFor="">Matériel</label>
                     <div className={`${s.container}`}>
                         {recipeState.materials.map(material => (
-                            <input onChange={(e) => recipeDispatch({type:"SET_MATERIAL", payload:{id:material.id, name:e.currentTarget.value}})} key={material.id} type="text" className={`${s.dark}`} value={material.name}/>
+                            <input required onChange={(e) => recipeDispatch({type:"SET_MATERIAL", payload:{id:material.id, name:e.currentTarget.value}})} key={material.id} type="text" className={`${s.dark}`} value={material.name}/>
                         ))}
                         <button onClick={() => recipeDispatch({type:"ADD_MATERIAL"})}>
                             <i className={`fa-solid fa-plus ${s.dark}`}></i>
@@ -167,10 +163,13 @@ export default function AddRecipeLayout(){
 
                     <label htmlFor="">Ingrédients</label>
                     <div className={`${s.container}`}>
+                        <div className={`${s.informations} ${s.ingredients}`}>
+                            <span>poid (g.)</span>
+                        </div>
                         {recipeState.ingredients.map(ingredient => (
                             <div key={ingredient.id} className={`${s.ingredient}`}>
-                                <input onChange={(e) => recipeDispatch({type:"SET_INGREDIENT_NAME", payload:{id:ingredient.id, name:e.currentTarget.value}})} className={`${s.name} ${s.dark}`} type="text" value={ingredient.name} />
-                                <input onChange={(e) => recipeDispatch({type:"SET_INGREDIENT_WEIGHT", payload:{id:ingredient.id, weight:parseInt(e.currentTarget.value)}})} className={`${s.weight} ${s.light}`} type="number" value={ingredient.weight ? ingredient.weight : ""} />
+                                <input required onChange={(e) => recipeDispatch({type:"SET_INGREDIENT_NAME", payload:{id:ingredient.id, name:e.currentTarget.value}})} className={`${s.name} ${s.dark}`} type="text" value={ingredient.name} />
+                                <input required onChange={(e) => recipeDispatch({type:"SET_INGREDIENT_WEIGHT", payload:{id:ingredient.id, weight:parseInt(e.currentTarget.value)}})} className={`${s.light}`} type="number" value={ingredient.weight ? ingredient.weight : ""} />
                             </div>
                         ))}
                         <button  onClick={() => recipeDispatch({type:"ADD_INGREDIENT"})}>
@@ -192,10 +191,10 @@ export default function AddRecipeLayout(){
                         {recipeState.instructions.map(instruction => (
                             <div key={instruction.id} className={`${s.step}`}>
                                 <input onChange={() => null} className={`${s.stepNumber} ${s.light}`} type="number" value={instruction.step} />
-                                <input onChange={(e) => recipeDispatch({type:"SET_INSTRUCTION_SENTENCE", payload:{id:instruction.id, sentence:e.currentTarget.value}})} className={`${s.instruction} ${s.dark}`} type="text" value={instruction.sentence} />
-                                <input onChange={(e) => recipeDispatch({type:"SET_INSTRUCTION_HOUR", payload:{id:instruction.id, value:e.currentTarget.value}})} className={`${s.light} ${s.duration}`} onBlur={addNullNumber} value={nullNumber(instruction.hour)} onClick={removeNullNumber} type="text" />
-                                <input onChange={(e) => recipeDispatch({type:"SET_INSTRUCTION_MINUTE", payload:{id:instruction.id, value:e.currentTarget.value}})} className={`${s.light} ${s.duration}`} onBlur={addNullNumber} value={nullNumber(instruction.minute)} onClick={removeNullNumber} type="text" />
-                                <input onChange={(e) => recipeDispatch({type:"SET_INSTRUCTION_SECONDE", payload:{id:instruction.id, value:e.currentTarget.value}})} className={`${s.light} ${s.duration}`} onBlur={addNullNumber} value={nullNumber(instruction.seconde)} onClick={removeNullNumber} type="text" />
+                                <input required onChange={(e) => recipeDispatch({type:"SET_INSTRUCTION_SENTENCE", payload:{id:instruction.id, sentence:e.currentTarget.value}})} className={`${s.instruction} ${s.dark}`} type="text" value={instruction.sentence} />
+                                <input onChange={(e) => recipeDispatch({type:"SET_INSTRUCTION_HOUR", payload:{id:instruction.id, value:e.currentTarget.value}})} className={`${s.light} ${s.duration}`} onBlur={addNullNumber} value={instruction.hour} onFocus={removeNullNumber} type="text" />
+                                <input onChange={(e) => recipeDispatch({type:"SET_INSTRUCTION_MINUTE", payload:{id:instruction.id, value:e.currentTarget.value}})} className={`${s.light} ${s.duration}`} onBlur={addNullNumber} value={instruction.minute} onFocus={removeNullNumber} type="text" />
+                                <input onChange={(e) => recipeDispatch({type:"SET_INSTRUCTION_SECONDE", payload:{id:instruction.id, value:e.currentTarget.value}})} className={`${s.light} ${s.duration}`} onBlur={addNullNumber} value={instruction.seconde} onFocus={removeNullNumber} type="text" />
                             </div>
                         ))}
                         <button onClick={() => recipeDispatch({type:"ADD_INSTRUCTION"})}>
@@ -206,6 +205,8 @@ export default function AddRecipeLayout(){
 
             </div>
 
-        </div>
+            
+
+        </form>
     )
 }
